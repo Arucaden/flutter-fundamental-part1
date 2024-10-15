86aaca49d9f03acfddd02773849c8f205bd5e5ab
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2024-10-15 20:50:41 +0700

    fixed pertemuan 6

--- README.md
+++ README.md
@@ -1,44 +1,90 @@
-### Nama : Daffa Maulana Satria
-### NIM : 2241720105
-### Kelas : TI-3D
+### Nama    : Daffa Maulana Satria
+### NIM     : 2241720105
+### Kelas   : TI-3D
 <br>
 
-# PERTEMUAN 5
-## Praktikum 2
-![alt text](img/image%20copy%209.png)
-
-## Praktikum 3
+# PERTEMUAN 6
+## Praktikum 1
+### Langkah 2
 ![alt text](img/image.png)
 
+### Langkah 4
+**Soal 1** 
+
+Letakkan widget Column di dalam widget Expanded agar menyesuaikan ruang yang tersisa di dalam widget Row. Tambahkan properti crossAxisAlignment ke CrossAxisAlignment.start sehingga posisi kolom berada di awal baris.
+
+```dart
+// soal 1
+child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+```
+
+**Soal 2**
+
+Letakkan baris pertama teks di dalam Container sehingga memungkinkan Anda untuk menambahkan padding = 8. Teks ‘Batu, Malang, Indonesia' di dalam Column, set warna menjadi abu-abu.
+
+```dart
+// soal 2
+Container(
+    padding: const EdgeInsets.only(bottom: 8),
+        child: const Text(
+            'Wisata Gunung di Batu',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+        ),
+    ),
+),
+Text(
+    'Batu, Malang, Indonesia',
+    style: TextStyle(
+        color: Colors.grey[500],
+    ),
+),
+```
+**Soal 3**
+
+Dua item terakhir di baris judul adalah ikon bintang, set dengan warna merah, dan teks "41". Seluruh baris ada di dalam Container dan beri padding di sepanjang setiap tepinya sebesar 32 piksel. Kemudian ganti isi body text ‘Hello World' dengan variabel titleSection
+
+```dart
+    // soal 3
+        Icon(
+            Icons.star,
+            color: Colors.red[500],
+        ),
+        const Text('41'),
+        ],
+    ),
+    );
+
+    return MaterialApp(
+        title: 'Flutter layout: Daffa Maulana Satria | 2241720105',
+        home: Scaffold(
+            appBar: AppBar(
+            title: const Text('Flutter layout demo'),
+            ),
+            body: Column(
+            children: [titleSection],
+            ),
+        ),
+    );
+```
+
+Hasil
 ![alt text](img/image%20copy.png)
 
-## Praktikum 4
+## Praktikum 2
+
 ![alt text](img/image%20copy%202.png)
+
+## Praktikum 3
 
 ![alt text](img/image%20copy%203.png)
 
+## Tugas Praktikum 1
 ![alt text](img/image%20copy%204.png)
 
-## Prakikum 5
+## Tugas Praktikum 2
 ![alt text](img/image%20copy%205.png)
+![alt text](img/image%20copy%206.png)
 
-![alt text](img/image%20copy%207.png)
 
-![alt text](img/image%20copy%208.png)
-
-## My First Code
-![alt text](img/image%20copy%2010.png)
-
-![alt text](img/image%20copy%2011.png)
-
-![alt text](img/image%20copy%2012.png)
-
-![alt text](img/image%20copy%2013.png)
-
-![alt text](img/image%20copy%2014.png)
-
-![alt text](img/image%20copy%2015.png)
-
-![alt text](img/image%20copy%2016.png)
-
-![alt text](img/image%20copy%2017.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,112 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  // ↓ Add the code below.
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600,  // ← Here.
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
         children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
+          Expanded(
+            // soal 1
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // soal 2
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung Everest',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Solukhumbu, Zona Sagarmatha, Nepal;Tingri County, Xigazê, Daerah Autonomous Tibet, Tiongkok',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
           ),
+          // soal 3
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
         ],
       ),
     );
-  }
-}
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
 
-  final WordPair pair;
+    Color color = Theme.of(context).primaryColor;
 
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
 
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Gunung Everest adalah gunung tertinggi kedua di dunia setelah Mauna Kea. Rabung puncaknya menandakan perbatasan antara Nepal dan Tibet; puncaknya berada di Tibet. Di Nepal, gunung ini disebut Sagarmatha dan dalam bahasa Tibet Chomolangma atau Qomolangma, dilafalkan dalam bahasa Tionghoa 珠穆朗瑪峰.'
+        '\n'
+        'Daffa Maulana Satria\n2241720105',
+        softWrap: true,
+      ),
+    );
 
-        // ↓ Make the following change.
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
+    return MaterialApp(
+      title: 'Flutter layout: Daffa Maulana Satria | 2241720105',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            Image.asset(
+              'img/Everest.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
     );
   }
 }
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
